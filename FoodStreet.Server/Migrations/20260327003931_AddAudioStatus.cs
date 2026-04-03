29ba9c483b82571b813b98b0988fa0848e86b3d5
Lê Trọng Duy <letrongduy113@gmail.com>
2026-04-03 17:14:34 +0700

    chore: update 20260327003931_AddAudioStatus.cs

---
+++ FoodStreet.Server/Migrations/20260327003931_AddAudioStatus.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodStreet.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddAudioStatus : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AudioStatus",
+                table: "Locations",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "AudioStatus",
+                value: "pending");
+
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "AudioStatus",
+                value: "pending");
+
+            migrationBuilder.UpdateData(
+                table: "Locations",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "AudioStatus",
+                value: "pending");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AudioStatus",
+                table: "Locations");
+        }
+    }
+}
