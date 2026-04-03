4204bb01f4777c30d8dacde64e9b93d1fef7df0d
Lê Trọng Duy <letrongduy113@gmail.com>
2026-04-03 17:14:25 +0700

    chore: update ITtsService.cs

--- SharedUI/Services/Interfaces/ITtsService.cs
+++ SharedUI/Services/Interfaces/ITtsService.cs
@@ -4,7 +4,7 @@ namespace FoodStreet.Client.Services
 {
     public interface ITtsService
     {
-        Task PlayTextAsync(string text);
+        Task PlayTextAsync(string text, string? languageCode = null);
         Task PlayAudioFileAsync(string url);
         Task StopAllAsync();
     }
