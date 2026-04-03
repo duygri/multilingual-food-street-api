56205af26e1910cae8b88ac5a99e238d37c79f64
Lê Trọng Duy <letrongduy113@gmail.com>
2026-04-03 17:14:14 +0700

    chore: update UserDtos.cs

--- FoodStreet.Server/DTOs/UserDtos.cs
+++ FoodStreet.Server/DTOs/UserDtos.cs
@@ -12,5 +12,5 @@ public class CreateUserRequest
 {
     public string Email { get; set; } = string.Empty;
     public string Password { get; set; } = string.Empty;
-    public string Role { get; set; } = "Seller";
+    public string Role { get; set; } = "POI Owner";
 }
